c9adcba00d1b5e122a29664329a7196c3193ebf9
yulin1118 <9800246+yulin1118@user.noreply.gitee.com>
2022-02-20 17:43:54 +0800

    修改终端状态分页样式

--- src/components/viewCharts/terminalStatus/TerminalCommunication.vue
+++ src/components/viewCharts/terminalStatus/TerminalCommunication.vue
@@ -1,31 +1,34 @@
 <template>
-  <div>
-    <table>
-      <thead>
-      <tr>
-        <th>IP</th>
-        <th>位置</th>
-        <th>流数</th>
-        <th>总发送字节数</th>
-        <th>名称</th>
-        <th>上次出现时间</th>
-        <th>上下行速率</th>
-        <th>总字节数</th>
-      </tr>
-      </thead>
-      <tr v-for="value in tableValue">
-        <td>{{ value.ip }}</td>
-        <td>{{ value.location }}</td>
-        <td>{{ value.flows }}</td>
-        <td>{{ value.totalrecvbytes + "bytes" }}</td>
-        <td>{{ value.name }}</td>
-        <td>{{ value.lastseen }}</td>
-        <td>{{ value.rates.toFixed(2) + "bit/s" }}</td>
-        <td>{{ value.totalbytes + "bytes" }}</td>
-      </tr>
+  <div style="background-color: antiquewhite">
+    <div class="top-table">
+      <table>
+        <thead>
+        <tr>
+          <th>IP</th>
+          <th>位置</th>
+          <th>流数</th>
+          <th>总发送字节数</th>
+          <th>名称</th>
+          <th>上次出现时间</th>
+          <th>上下行速率</th>
+          <th>总字节数</th>
+        </tr>
+        </thead>
+        <tr v-for="value in tableValue">
+          <td>{{ value.ip }}</td>
+          <td>{{ value.location }}</td>
+          <td>{{ value.flows }}</td>
+          <td>{{ value.totalrecvbytes + "bytes" }}</td>
+          <td>{{ value.name }}</td>
+          <td>{{ value.lastseen }}</td>
+          <td>{{ value.rates.toFixed(2) + "bit/s" }}</td>
+          <td>{{ value.totalbytes + "bytes" }}</td>
+        </tr>
 
-    </table>
-    <div style="text-align: center">
+      </table>
+    </div>
+
+    <div class="page-style">
       <el-pagination
           @size-change="handleSizeChange"
           @current-change="handleCurrentChange"
@@ -83,14 +86,10 @@ table{
   border-collapse: collapse;
   position: absolute;
   text-align: center;
+  /*background-color: blueviolet;*/
+  top: 1px;
 }
-table thead th{
-  color: #61d2f7;
-  font-size: 14px;
-  font-weight: 600;
-  padding-top: 5px;
-  padding-bottom: 5px;
-}
+
 table tr{
   color: #61d2f7;
   font-size: 14px;
@@ -108,4 +107,29 @@ table thead th {
 td {
   color: 	#F5F5F5;
 }
+.top-table{
+  /*background-color: #4b8df8;*/
+  position:relative;
+}
+.page-style{
+  text-align: center;
+/*background-color: #61d2f7;*/
+  position: absolute;
+  bottom: 1px;
+  left: 300px;
+}
+
+/deep/ .el-pager li{
+  background-color:#10121A;
+}
+/deep/ .el-pagination .btn-prev {
+  background-color:#10121A;
+}
+/deep/ .el-pagination .btn-next {
+  background-color: #10121A;
+}
+/deep/ .el-pagination {
+  color:#606266;
+}
+
 </style>
